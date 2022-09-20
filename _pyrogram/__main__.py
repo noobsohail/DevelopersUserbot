4bb2983ceeaaf2c884fb26ba7ae7941590f2c39a
ꜱōʜᴇʀᴜ ᴋāɴ (AKA) ꜱᴏʜᴀɪʟ ᴋʜᴀɴ <87354331+noobsohail@users.noreply.github.com>
2022-09-21 00:53:08 +0530

    Update __main__.py

--- _pyrogram/__main__.py
+++ _pyrogram/__main__.py
@@ -4,7 +4,10 @@ from pyrogram import idle, Client, filters
 from _pyrogram import app, LOGGER
 from _pyrogram.modules import *
 import os
+import asyncio
 
 print("Pyrogram User Client is Start")
 app.start()
 print(app.get_me)
+loop = asyncio.get_event_loop()
+loop.run_forever()    
